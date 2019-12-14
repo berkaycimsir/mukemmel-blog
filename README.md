041cd3e2172e22c8c81ed08fcf4c6cbd591b89bb
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-14 22:47:40 +0300

    Initing apollo server and readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This project forked from [Selman Kahya](https://github.com/SelmanKahya/mukemmel-
 
 The backend server of this project: [Backend (graphql playground)](https://mukemmel-blog-backend.herokuapp.com/graphql)
 
-This project deployed to the now: [Frontend](https://mukemmel-blog-frontend.now.sh/)
+This project deployed to the now: [Frontend](https://mukemmel-blog-frontend.berkaycimsir.now.sh)
--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,5 +1,34 @@
 import * as React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-const Home: React.FC = () => <h1>home</h1>;
+interface QueryData {
+  users: [any];
+}
+
+const query = gql`
+  query {
+    users {
+      id
+      name
+      surname
+      username
+      email
+      createdAt
+    }
+  }
+`;
+
+const Home: React.FC = () => {
+  const { data, loading } = useQuery<QueryData>(query);
+
+  if (loading) return <div>loading</div>;
+
+  return (
+    <div>
+      <h1>home</h1> <p>{data.users.map(user => user.name)}</p>
+    </div>
+  );
+};
 
 export default Home;
