db498d0fceb18d02415e5815154e95034911f4f7
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-18 12:20:19 +0300

    typescript types defined for get most trend blog query

--- @types/interfaces/PageInterfaces/Home/trendblogs.interfaces.tsx
+++ @types/interfaces/PageInterfaces/Home/trendblogs.interfaces.tsx
@@ -3,3 +3,8 @@ import { Blog } from "../../../types/Blog";
 export interface GetTrendBlogsReturnType {
   getTrendBlogs: [Blog];
 }
+
+export interface GetMostTrendBlogReturnType {
+  blog: Blog;
+  errorMessage: string;
+}
