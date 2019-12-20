8731205172097e6b7149de5af34bf30c3707e56b
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-20 20:47:00 +0300

    designment

--- pages/BlogDetails/BlogDetailsCard.tsx
+++ pages/BlogDetails/BlogDetailsCard.tsx
@@ -1,21 +1,33 @@
 import * as React from "react";
 import { Card, Divider, Image } from "semantic-ui-react";
 import { Props } from "../../@types/interfaces/PageInterfaces/BlogDetails/blogdetailscard.interfaces";
-import { useEffect } from "react";
+import Moment from "react-moment";
 
 const BlogDetailsCard: React.FC<Props> = ({ blog }) => {
   const { id, title, content, img, views, tags, likes, createdAt } = blog;
+
+  const sortedTags: Array<string> = tags.slice(0, 2);
 
   return (
     <Card className="blog-card" color="violet" fluid centered>
       <Image src={img} />
       <Card.Content>
-        <Card.Header>{title}</Card.Header>
+        <h1 className="blog-details-title">
+          {title}{" "}
+          <span style={{ float: "right", fontSize: "13px", opacity: ".7" }}>
+            <Moment toNow ago date={createdAt} /> ago
+          </span>
+        </h1>
+
         <Divider />
         <Card.Description
           dangerouslySetInnerHTML={{ __html: content }}
           className="blog-detail-content"
         />
+      </Card.Content>
+      <Card.Content extra>
+        {sortedTags.map(tag => ` #${tag}`)} ...
+        <span style={{ float: "right" }}>{views} görüntülenme</span>
       </Card.Content>
     </Card>
   );
