dfc42790b402c47de35a5ae6aef5ceef92b4ea58
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-22 14:22:43 +0300

    Comment's user image changes by user's gender, for men image is men images, for women image is women images

--- pages/BlogDetails/Comments.tsx
+++ pages/BlogDetails/Comments.tsx
@@ -4,13 +4,38 @@ import { Comment, Icon } from "semantic-ui-react";
 import Moment from "react-moment";
 
 const Comments: React.FC<Props> = ({ comments }) => {
+  const menGenderImageUrls: Array<string> = [
+    "https://react.semantic-ui.com/images/avatar/small/matt.jpg",
+    "https://react.semantic-ui.com/images/avatar/small/elliot.jpg",
+    "https://react.semantic-ui.com/images/avatar/small/joe.jpg",
+    "https://react.semantic-ui.com/images/avatar/small/christian.jpg",
+    "https://react.semantic-ui.com/images/avatar/small/steve.jpg"
+  ];
+
+  const womenGenderImageUrls: Array<string> = [
+    "https://react.semantic-ui.com/images/avatar/small/stevie.jpg",
+    "https://react.semantic-ui.com/images/avatar/small/jenny.jpg"
+  ];
+
+  const getImageUrlByGender: Function = (gender: string): string => {
+    if (gender === "men") {
+      const randomIndexForMenGender: number = Math.floor(Math.random() * 5);
+      return menGenderImageUrls[randomIndexForMenGender];
+    }
+
+    if (gender === "women") {
+      const randomIndexForWomenGender: number = Math.floor(Math.random() * 2);
+      return womenGenderImageUrls[randomIndexForWomenGender];
+    }
+  };
+
   return (
     <Comment.Group>
       {comments.map(comment => (
         <Comment key={comment.id}>
           <Comment.Avatar
             as="a"
-            src="https://react.semantic-ui.com/images/avatar/small/stevie.jpg"
+            src={getImageUrlByGender(comment.user.gender)}
           />
           <Comment.Content>
             <Comment.Author>
