f5e761f283c030291ed0e5cfd624693c74d99a8f
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-22 23:56:08 +0300

    add comment mutation wrote

---
+++ graphql/Comment/mutation.ts
@@ -0,0 +1,14 @@
+import { gql } from "apollo-boost";
+
+export const ADD_COMMENT = gql`
+  mutation($blog_id: ID!, $user_id: ID!, $content: String!) {
+    addComment(
+      data: { blog_id: $blog_id, user_id: $user_id, content: $content }
+    ) {
+      comment {
+        content
+      }
+      errorMessage
+    }
+  }
+`;
--- pages/BlogDetails/Comment/CommentItem.tsx
+++ pages/BlogDetails/Comment/CommentItem.tsx
@@ -4,29 +4,16 @@ import { Comment } from "semantic-ui-react";
 import Moment from "react-moment";
 
 const CommentItem: React.FC<Props> = ({ activeUser, comment }) => {
-  const menGenderImageUrls: Array<string> = [
-    "https://react.semantic-ui.com/images/avatar/small/matt.jpg",
-    "https://react.semantic-ui.com/images/avatar/small/elliot.jpg",
-    "https://react.semantic-ui.com/images/avatar/small/joe.jpg",
-    "https://react.semantic-ui.com/images/avatar/small/christian.jpg",
-    "https://react.semantic-ui.com/images/avatar/small/steve.jpg"
+  const menGenderImageUrl: Array<string> = [
+    "https://react.semantic-ui.com/images/avatar/small/joe.jpg"
   ];
 
-  const womenGenderImageUrls: Array<string> = [
-    "https://react.semantic-ui.com/images/avatar/small/stevie.jpg",
-    "https://react.semantic-ui.com/images/avatar/small/jenny.jpg"
+  const womenGenderImageUrl: Array<string> = [
+    "https://react.semantic-ui.com/images/avatar/small/stevie.jpg"
   ];
 
   const getImageUrlByGender: Function = (gender: string): string => {
-    if (gender === "men") {
-      const randomIndexForMenGender: number = Math.floor(Math.random() * 5);
-      return menGenderImageUrls[randomIndexForMenGender];
-    }
-
-    if (gender === "women") {
-      const randomIndexForWomenGender: number = Math.floor(Math.random() * 2);
-      return womenGenderImageUrls[randomIndexForWomenGender];
-    }
+    return gender === "men" ? menGenderImageUrl[0] : womenGenderImageUrl[0];
   };
 
   return (
