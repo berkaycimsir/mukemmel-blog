b25035bd89b3417c1076988fa23dae8bb1854d13
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-23 00:14:54 +0300

    AddComment: Textarea change event created and react hooks(useState) used for handling textarea value

--- pages/BlogDetails/Comment/AddComment.tsx
+++ pages/BlogDetails/Comment/AddComment.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 import { useMutation } from "react-apollo";
 import {
@@ -9,6 +10,8 @@ import { ADD_COMMENT } from "../../../graphql/Comment/mutation";
 import { GET_BLOG_BY_ID } from "../../../graphql/Blog/query";
 
 const AddComment: React.FC = () => {
+  const [content, setContent] = useState<string>("");
+
   const [addComment, { loading }] = useMutation<
     AddCommentReturnData,
     AddCommentVariables
@@ -26,6 +29,10 @@ const AddComment: React.FC = () => {
       <Form.TextArea
         style={{ minHeight: "15em" }}
         placeholder="Enter your comment here..."
+        value={content}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setContent(e.target.value);
+        }}
       />
       <Button content="Add Comment" primary />
     </Form>
