6e2afc3cccf47b264b9bbaf6fe0ca6c5c83efa28
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-22 18:56:46 +0300

    if there is an active user card changed to segment and some designment

--- components/Login/IfThereIsAnActiveUser.tsx
+++ components/Login/IfThereIsAnActiveUser.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Card, Message, Divider } from "semantic-ui-react";
+import { Card, Message, Divider, Segment, Header } from "semantic-ui-react";
 import Logout from "../Logout/Logout";
 import Moment from "react-moment";
 
@@ -11,12 +11,12 @@ const IfThereIsAnActiveUser: React.FC<Props> = ({ session }) => {
   const { name, surname, username, createdAt } = session.activeUser.user;
 
   return (
-    <Card fluid className="blog-card">
+    <Segment color="blue" fluid className="blog-card">
       <Card.Content>
-        <Card.Header textAlign="center" className="if-active-user-title">
+        <Header textAlign="center" className="if-active-user-title">
           Hoşgeldin, {name} {surname}
-        </Card.Header>
-        <Card.Meta className="if-active-user-meta">
+        </Header>
+        <Card.Meta style={{ opacity: 0.7 }} className="if-active-user-meta">
           <span>@{username}</span>
           <span style={{ float: "right" }}>
             <Moment date={createdAt} fromNow ago /> ago
@@ -30,8 +30,9 @@ const IfThereIsAnActiveUser: React.FC<Props> = ({ session }) => {
           </Message>
         </Card.Description>
       </Card.Content>
+      <Divider />
       <Card.Content extra content={<Logout />} />
-    </Card>
+    </Segment>
   );
 };
 
