ba754c789bc9e1bca4a9091e9b8c52c2a2a37b8d
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-23 16:19:17 +0300

    textarea value handling completed

--- pages/BlogDetails/Comment/UpdateComment.tsx
+++ pages/BlogDetails/Comment/UpdateComment.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
+import { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
-import { Props } from "../../../@types/interfaces/PageInterfaces/BlogDetails/updatecomment.intrefaces";
+import {
+  Props,
+  UpdateCommentReturnData,
+  UpdateCommentVariables
+} from "../../../@types/interfaces/PageInterfaces/BlogDetails/updatecomment.intrefaces";
 
 const UpdateComment: React.FC<Props> = ({ blog_id, user_id, id }) => {
+  const [content, setContent] = useState<string>("");
+
   return (
     <Form reply>
       <Form.TextArea
         style={{ minHeight: "12em" }}
         placeholder="Enter your comment"
+        value={content}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setContent(e.target.value);
+        }}
       />
       <Button type="submit" content="Update Comment" primary />
     </Form>
