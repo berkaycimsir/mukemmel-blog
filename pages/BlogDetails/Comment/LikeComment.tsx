6138c0599579fdd1b49c23dfae294c32f1f77ae9
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-23 17:20:25 +0300

    like comment design completed

--- pages/BlogDetails/Comment/LikeComment.tsx
+++ pages/BlogDetails/Comment/LikeComment.tsx
@@ -1,10 +1,24 @@
 import * as React from "react";
+import { useState } from "react";
 import { Props } from "../../../@types/interfaces/PageInterfaces/BlogDetails/likecomment.interfaces";
 
 const LikeComment: React.FC<Props> = ({ id }) => {
-  // <img src="https://img.icons8.com/dusk/64/000000/like.png">
+  const [isLiked, setIsLiked] = useState<boolean>(false);
 
-  return <img src="https://img.icons8.com/wired/25/000000/like.png" />;
+  const getImgUrl: Function = (): string => {
+    if (isLiked === true) {
+      return "https://img.icons8.com/dusk/25/000000/like.png";
+    }
+    return "https://img.icons8.com/wired/25/000000/like.png";
+  };
+
+  return (
+    <img
+      style={{ marginLeft: "3px" }}
+      onClick={() => setIsLiked(!isLiked)}
+      src={getImgUrl()}
+    />
+  );
 };
 
 export default LikeComment;
