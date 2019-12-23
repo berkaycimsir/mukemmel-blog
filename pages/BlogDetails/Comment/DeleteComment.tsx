4bd10a0dd4e2dcd9963934577b655798236e1c3b
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-23 13:45:50 +0300

    delete comment useMutation hooks wrote

--- pages/BlogDetails/Comment/DeleteComment.tsx
+++ pages/BlogDetails/Comment/DeleteComment.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
-import { Props } from "../../../@types/interfaces/PageInterfaces/BlogDetails/deletecomment.interfaces";
+import {
+  Props,
+  DeleteCommentReturnData,
+  DeleteCommentVariables
+} from "../../../@types/interfaces/PageInterfaces/BlogDetails/deletecomment.interfaces";
 import { Button } from "semantic-ui-react";
+import { useMutation } from "react-apollo";
+import { DELETE_COMMENT } from "../../../graphql/Comment/mutation";
 
 const DeleteComment: React.FC<Props> = ({ id }) => {
+  const [deleteComment, { loading }] = useMutation<
+    DeleteCommentReturnData,
+    DeleteCommentVariables
+  >(DELETE_COMMENT, {
+    variables: { id }
+  });
+
   return <Button size="mini" color="red" basic content="Delete" />;
 };
 
