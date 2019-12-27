7da42dff45f81cb72dcb36d6dbd056b2383b8c55
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-27 19:00:36 +0300

    Home Page -> TrendBlogs part now displaying by choosen categories

---
+++ pages/Home/TrendBlogs/CategoryBlogs.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { Tab, Grid, Item } from "semantic-ui-react";
+import { useQuery } from "react-apollo";
+import { GET_BLOGS_BY_CATEGORY } from "../../../graphql/Blog/query";
+import Loading from "../../../components/Loading/Loading";
+import BlogCard from "../BlogCard";
+import {
+  GetBlogsByCategoryReturnData,
+  GetBlogsByCategoryVariables
+} from "../../../@types/interfaces/PageInterfaces/Categories/category.interfaces";
+import { Blog } from "../../../@types/types/DatabaseTypes";
+
+type Props = {
+  category: string;
+};
+
+const CategoryBlogs: React.FC<Props> = ({ category }) => {
+  const { data: getBlogsData, loading: getBlogsLoading } = useQuery<
+    GetBlogsByCategoryReturnData,
+    GetBlogsByCategoryVariables
+  >(GET_BLOGS_BY_CATEGORY, {
+    variables: { category }
+  });
+
+  if (getBlogsLoading) {
+    return <Loading size={50} />;
+  }
+
+  const mostTrendBlogViews: number = Math.max.apply(
+    Math,
+    getBlogsData.getBlogByCategory.map(blog => blog.views)
+  );
+
+  const mostTrendBlog: Blog = getBlogsData.getBlogByCategory.find(
+    blog => blog.views === mostTrendBlogViews
+  );
+
+  const blogs: Array<Blog> = getBlogsData.getBlogByCategory.slice(0, 4);
+
+  return (
+    <>
+      <Grid columns={2} doubling>
+        <Grid.Column width={8}>
+          <BlogCard user={mostTrendBlog.user} blog={mostTrendBlog} />
+        </Grid.Column>
+        <Grid.Column>
+          <Grid columns={4} doubling>
+            {blogs.map(blog => {
+              if (blog.title !== mostTrendBlog.title) {
+                return (
+                  <Grid.Column key={blog.id} widescreen={8}>
+                    <BlogCard user={blog.user} key={blog.id} blog={blog} />
+                  </Grid.Column>
+                );
+              }
+            })}
+          </Grid>
+        </Grid.Column>
+      </Grid>
+    </>
+  );
+};
+
+export default CategoryBlogs;
--- pages/Home/TrendBlogs/TrendBlogs.tsx
+++ pages/Home/TrendBlogs/TrendBlogs.tsx
@@ -1,36 +1,76 @@
 import * as React from "react";
-import {
-  Tab,
-  SemanticShorthandItem,
-  TabPaneProps,
-  Card,
-  Divider
-} from "semantic-ui-react";
+import { Card, Divider, Menu } from "semantic-ui-react";
 import AllTrendBlogsPane from "./AllTrendBlogsPane";
+import CategoryDropdown from "./CategoryDropdown";
+import CategoryBlogs from "./CategoryBlogs";
 
-const panes: {
-  pane?: SemanticShorthandItem<TabPaneProps>;
-  menuItem?: any;
-  render?: () => React.ReactNode;
-}[] = [
-  {
-    menuItem: { key: "all-trend-blogs", content: "Tümü" },
-    render: () => <AllTrendBlogsPane />
-  },
-  {
-    menuItem: "Tab 3",
-    render: () => <Tab.Pane attached="top">Trend İçerikler</Tab.Pane>
-  }
-];
+const TrendBlogs: React.FC = () => {
+  const [activeItem, setActiveItem] = React.useState<string>("all");
+  const [windowWidth, setWindowWidth] = React.useState<number>(
+    window.innerWidth
+  );
 
-const TrendBlogs: React.FC = () => (
-  <Card style={{ marginTop: "20px" }} fluid color="teal">
-    <Card.Content>
-      <Card.Header style={{ marginTop: "3px" }}>Trend İçerikler</Card.Header>
-      <Divider />
-      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-    </Card.Content>
-  </Card>
-);
+  React.useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, [windowWidth, setWindowWidth]);
+
+  return (
+    <Card style={{ marginTop: "20px" }} fluid color="teal">
+      <Card.Content>
+        <Card.Header style={{ marginTop: "3px" }}>Trend İçerikler</Card.Header>
+        <Divider />
+        <Menu pointing>
+          <Menu.Item
+            content="Tümü"
+            active={activeItem === "all"}
+            onClick={() => setActiveItem("all")}
+          />
+          <Menu.Item
+            content="Javascript"
+            active={activeItem === "javascript"}
+            onClick={() => setActiveItem("javascript")}
+          />
+          {windowWidth > 766.9 ? (
+            <>
+              <Menu.Item
+                content="Php"
+                active={activeItem === "php"}
+                onClick={() => setActiveItem("php")}
+              />
+              <Menu.Item
+                content="Python"
+                active={activeItem === "python"}
+                onClick={() => setActiveItem("python")}
+              />
+              <Menu.Item
+                content="Html"
+                active={activeItem === "html"}
+                onClick={() => setActiveItem("html")}
+              />
+              <Menu.Item
+                content="C#"
+                active={activeItem === "csharp"}
+                onClick={() => setActiveItem("csharp")}
+              />
+            </>
+          ) : (
+            <Menu.Menu position="right">
+              <CategoryDropdown
+                activeItem={activeItem}
+                setActiveItem={setActiveItem}
+              />
+            </Menu.Menu>
+          )}
+        </Menu>
+
+        {activeItem === "all" ? (
+          <AllTrendBlogsPane />
+        ) : (
+          <CategoryBlogs category={activeItem} />
+        )}
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default TrendBlogs;
