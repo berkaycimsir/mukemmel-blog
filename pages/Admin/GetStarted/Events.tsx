c8effde86822e65e9aaedad1f983f92d362fe842
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-29 17:10:34 +0300

    Admin -> Event part, get last registered users

--- pages/Admin/GetStarted/Events.tsx
+++ pages/Admin/GetStarted/Events.tsx
@@ -10,8 +10,10 @@ import { GET_BLOGS } from "../../../graphql/Blog/query";
 import Loading from "../../../components/Loading/Loading";
 import { GET_COMMENTS } from "../../../graphql/Comment/query";
 import Moment from "react-moment";
-import { Blog, Comment } from "../../../@types/types/DatabaseTypes";
+import { Blog, Comment, User } from "../../../@types/types/DatabaseTypes";
 import { GetCommentsReturnData } from "../../../@types/interfaces/PageInterfaces/BlogDetails/allcommentstab.interfaces";
+import { GET_LAST_USERS } from "../../../graphql/User/query";
+import { GetUsersReturnData } from "../../../@types/interfaces/PageInterfaces/Admin/GetStarted/events.interfaces";
 
 const Events: React.FC = () => {
   const { data: getBlogsData, loading: getBlogsLoading } = useQuery<
@@ -22,6 +24,10 @@ const Events: React.FC = () => {
     GetCommentsReturnData
   >(GET_COMMENTS);
 
+  const { data: getUsersData, loading: getUsersLoading } = useQuery<
+    GetUsersReturnData
+  >(GET_LAST_USERS);
+
   let slicedBlogs: Array<Blog>;
 
   if (!getBlogsLoading) slicedBlogs = getBlogsData.blogs.slice(0, 3);
@@ -31,6 +37,10 @@ const Events: React.FC = () => {
   if (!getCommentsLoading) {
     slicedComments = getCommentsData.comments.slice(0, 3);
   }
+
+  let slicedUsers: Array<User>;
+
+  if (!getUsersLoading) slicedUsers = getUsersData.users.slice(0, 3);
 
   const menGenderImageUrl: Array<string> = [
     "https://react.semantic-ui.com/images/avatar/small/joe.jpg"
@@ -86,6 +96,26 @@ const Events: React.FC = () => {
                     <Moment date={comment.createdAt} fromNow ago /> ago
                   </Feed.Date>
                   <Feed.Summary>{comment.content}</Feed.Summary>
+                </Feed.Content>
+              </Feed.Event>
+            ))}
+          </Feed>
+
+          <Feed>
+            <Header as="h4" content="Son kayıtlar:" />
+            {slicedUsers.map((user: User) => (
+              <Feed.Event>
+                <Feed.Label>
+                  <Image size="mini" src={getImageUrlByGender(user.gender)} />
+                </Feed.Label>
+                <Feed.Content>
+                  <Feed.Date>
+                    @{user.username} &nbsp;
+                    <Moment date={user.createdAt} fromNow ago /> ago
+                  </Feed.Date>
+                  <Feed.Summary>
+                    {user.name} {user.surname}
+                  </Feed.Summary>
                 </Feed.Content>
               </Feed.Event>
             ))}
