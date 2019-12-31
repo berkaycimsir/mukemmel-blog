e390c058efb9849b3870a28bf1700628f6daab65
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-31 23:04:08 +0300

    typescript types and interfaces wrote for add blog mutation

--- @types/interfaces/PageInterfaces/Admin/Articles/AddBlog/blogaccordion.interfaces.tsx
+++ @types/interfaces/PageInterfaces/Admin/Articles/AddBlog/blogaccordion.interfaces.tsx
@@ -1,3 +1,24 @@
+import { Blog } from "../../../../../types/database/DatabaseTypes";
+
 export type Props = {
   content: string;
 };
+
+type ReturnType = {
+  blog: Blog;
+  errorMessage: string;
+};
+
+export interface AddBlogMutationReturnData {
+  createBlog: ReturnType;
+}
+
+export interface AddBlogMutationVariables {
+  owner_id: string;
+  title: string;
+  content: string;
+  summary: string;
+  tags: Array<string>;
+  img: string;
+  category: string;
+}
