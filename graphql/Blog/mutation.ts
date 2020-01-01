e98d8c764d4cdd3963666eae3fc28eafabfb1090
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2020-01-01 18:37:29 +0300

    update blog mutation query wrote

--- graphql/Blog/mutation.ts
+++ graphql/Blog/mutation.ts
@@ -30,6 +30,35 @@ export const ADD_BLOG = gql`
   }
 `;
 
+export const UPDATE_BLOG = gql`
+  mutation(
+    $blog_id: ID!
+    $title: String
+    $content: String
+    $summary: String
+    $tags: [String!]
+    $img: String
+    $category: String
+  ) {
+    createBlog(
+      data: {
+        blog_id: $blog_id
+        title: $title
+        content: $content
+        summary: $summary
+        tags: $tags
+        img: $img
+        category: $category
+      }
+    ) {
+      blog {
+        summary
+      }
+      errorMessage
+    }
+  }
+`;
+
 export const DELETE_BLOG = gql`
   mutation($id: ID!) {
     deleteBlog(id: $id) {
