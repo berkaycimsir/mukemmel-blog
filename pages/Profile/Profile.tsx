a5a677cbb9d877482be970b4f803c377597e10a4
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2020-01-23 18:41:41 +0300

    get user by id query wrote in profile page

--- pages/Profile/Profile.tsx
+++ pages/Profile/Profile.tsx
@@ -1,6 +1,24 @@
 import * as React from "react";
+import { useQuery } from "react-apollo";
+import {
+  GetUserByIdReturnData,
+  GetUserByIdVariables
+} from "../../@types/interfaces/PageInterfaces/Profile/profile.interfaces";
+import { USER } from "../../graphql/User/query";
+import Loading from "../../components/Loading/Loading";
 
 const Profile: React.FC = () => {
+  const { data, loading } = useQuery<
+    GetUserByIdReturnData,
+    GetUserByIdVariables
+  >(USER, {
+    variables: { id: window.location.pathname.split("/")[2] }
+  });
+
+  if (loading) return <Loading size={50} />;
+
+  console.log(data);
+
   return <div>profile page</div>;
 };
 
