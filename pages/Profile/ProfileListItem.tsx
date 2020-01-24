4d4a8b2b43fec81ba69400d16f71af40c281d698
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2020-01-25 00:33:00 +0300

    icons added to profile list item component for update user

--- pages/Profile/ProfileListItem.tsx
+++ pages/Profile/ProfileListItem.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
-import { List } from "semantic-ui-react";
+import { List, Image } from "semantic-ui-react";
+import {
+  EditOutlined,
+  DoneOutline,
+  CloseOutlined,
+  Done
+} from "@material-ui/icons";
 
 type Props = {
   color: string;
@@ -8,11 +14,51 @@ type Props = {
 };
 
 const ProfileListItem: React.FC<Props> = ({ color, header, description }) => {
+  const [update, setUpdate] = React.useState<{
+    updating?: boolean;
+    field?: string;
+    text?: string;
+  }>({ updating: false, text: "" });
+
+  const { updating, field, text } = update;
+
   return (
     <List.Item style={{ marginTop: "5px" }} as="a">
       <List.Content>
         <List.Header style={{ color }} content={header} />
-        <List.Description>{description}</List.Description>
+        <List.Description>
+          {description}
+
+          {!updating && (
+            <Image
+              onClick={() => {
+                setUpdate({ updating: !updating });
+              }}
+              disabled={updating && !text}
+              style={{ float: "right" }}
+              children={<EditOutlined />}
+            />
+          )}
+          {updating && (
+            <Image
+              onClick={() => {
+                text && setUpdate({ updating: false });
+              }}
+              disabled={!text}
+              style={{ float: "right" }}
+              children={<Done htmlColor="green" />}
+            />
+          )}
+          {updating && (
+            <Image
+              onClick={() => {
+                setUpdate({ updating: false });
+              }}
+              style={{ float: "right" }}
+              children={<CloseOutlined htmlColor="red" />}
+            />
+          )}
+        </List.Description>
       </List.Content>
     </List.Item>
   );
