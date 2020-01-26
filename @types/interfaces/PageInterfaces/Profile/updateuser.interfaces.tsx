47ddedbd2eb0159143fbf17e59a7b7c54e4451d9
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2020-01-27 01:57:06 +0300

    typescript interfaces has wrote for update user mutation

--- @types/interfaces/PageInterfaces/Profile/updateuser.interfaces.tsx
+++ @types/interfaces/PageInterfaces/Profile/updateuser.interfaces.tsx
@@ -1,3 +1,16 @@
 export type Props = {
   setUpdating: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+export interface UpdateUserReturnData {
+  update: boolean;
+}
+
+export interface UpdateUserVariables {
+  id: string;
+  name?: string;
+  surname?: string;
+  username?: string;
+  email?: string;
+  gender?: string;
+}
